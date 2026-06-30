9dbd6ded92c14b9dfe839846db2a84ec1a8214e0
ToniGrbic <tonigrbic.5@gmail.com>
2026-07-01 00:47:48 +0200

    barrel export

---
+++ src/components/products/index.ts
@@ -0,0 +1,5 @@
+export { default as ImageGallery } from "./ImageGallery";
+export { default as Pagination } from "./Pagination";
+export { default as ProductCard } from "./ProductCard";
+export { default as ProductFilters } from "./ProductFilters";
+export { default as ProductTable } from "./ProductTable";
