2fb81c9d16cbcb6908448ba0b9b984e8ce2e9295
ToniGrbic <tonigrbic.5@gmail.com>
2026-06-30 23:56:15 +0200

    barrel export common components

---
+++ src/components/common/index.ts
@@ -0,0 +1,9 @@
+export { default as ErrorBoundary } from "../ErrorBoundary";
+export { default as Button } from "./Button";
+export { default as Input } from "./Input";
+export {
+  default as Skeleton,
+  ProductGridSkeleton,
+  ProductCardSkeleton,
+} from "./Skeleton";
+export { default as StateMessage } from "./StateMessage";
