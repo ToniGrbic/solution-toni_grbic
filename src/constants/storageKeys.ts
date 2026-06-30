24a04fc2b077bede36355d833f528b4726a8049d
ToniGrbic <tonigrbic.5@gmail.com>
2026-07-01 01:14:59 +0200

    constants

---
+++ src/constants/storageKeys.ts
@@ -0,0 +1,7 @@
+const TOKEN_KEY = "token";
+const USER_KEY = "user";
+const THEME_KEY = "theme";
+const FAVORITES_KEY = "favorites";
+const SCROLL_KEY = "products-scroll";
+
+export { FAVORITES_KEY, SCROLL_KEY, THEME_KEY, TOKEN_KEY, USER_KEY };
