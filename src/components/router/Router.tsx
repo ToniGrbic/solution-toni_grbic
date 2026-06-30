1c75b3ece9d8b5896d2472209e771b37d1295071
ToniGrbic <tonigrbic.5@gmail.com>
2026-07-01 00:09:42 +0200

    router pages

--- src/components/router/Router.tsx
+++ src/components/router/Router.tsx
@@ -1,12 +1,35 @@
-import Home from "@/pages/Home";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { Routes as AppRoutes } from "./routes";
+import Layout from "@/components/layout/Layout";
+import Favorites from "@/pages/Favorites";
+import Login from "@/pages/Login";
+import NotFound from "@/pages/NotFound";
+import ProductDetail from "@/pages/ProductDetail";
+import Products from "@/pages/Products";
+import { Routes as AppRoutes } from "@/types/enums";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import ProtectedRoute from "../auth/ProtectedRoute";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={AppRoutes.HOME} element={<Home />} />
+        <Route element={<Layout />}>
+          <Route
+            path={AppRoutes.HOME}
+            element={<Navigate to={AppRoutes.PRODUCTS} replace />}
+          />
+          <Route path={AppRoutes.PRODUCTS} element={<Products />} />
+          <Route path={AppRoutes.PRODUCT_DETAIL} element={<ProductDetail />} />
+          <Route path={AppRoutes.LOGIN} element={<Login />} />
+          <Route
+            path={AppRoutes.FAVORITES}
+            element={
+              <ProtectedRoute>
+                <Favorites />
+              </ProtectedRoute>
+            }
+          />
+          <Route path={AppRoutes.NOT_FOUND} element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
