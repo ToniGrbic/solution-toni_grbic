4eb6b5013bdc1f85929c803da86cf6240898b4b1
ToniGrbic <tonigrbic.5@gmail.com>
2026-06-30 15:55:32 +0200

    react query and axios setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Router from "./compoments/router/Router";
+import Router from "./components/router/Router";
+import "./styles/global.scss";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router />
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
   </StrictMode>,
 );
